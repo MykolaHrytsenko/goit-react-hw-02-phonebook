25a1c311bd3f6d1765a6375e94647b0f2e5a32ed
MykolaHrytsenko <niksandler@gmail.com>
2022-07-30 22:06:01 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleInputChange = event => {
+    // console.log(event.currentTarget.value);
+
+    this.setState({ name: event.currentTarget.value });
+  };
+
+  render() {
+    return (
+      <div>
+  <h1>Phonebook</h1>
+  <ContactForm ... />
+
+  <h2>Contacts</h2>
+  <Filter ... />
+  <ContactList ... />
+</div>
+    );
+  }
+}
